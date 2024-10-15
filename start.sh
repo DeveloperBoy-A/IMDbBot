0dffd05205194993a8b74808e8e97ca2bd9434f5
𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫_𝐁𝐨𝐲 <smartmeenaankit786@gmail.com>
2024-10-15 17:14:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b koyeb https://github.com/JOSProjects/IMDb-Movie-Bot.git /IMDb-Movie-Bot
-cd /IMDb-Movie-Bot
+git clone -b koyeb https://github.com/DeveloperBoy-A/IMDbBot.git /IMDbBot
+cd /IMDbBot
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
